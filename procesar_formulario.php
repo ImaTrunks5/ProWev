5799ba0a26b6ecf78aed5877e5d9c95f4256875d
AdolfoAngl <152242930+AdolfoAngl@users.noreply.github.com>
2024-01-06 01:22:31 -0600

    Ya obtiene datos de la BD

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -1,11 +1,36 @@
 <?php
-$conexion = mysqli_connect("localhost","root","","bdformulario"); // Conexión a la base de datos
+include("conexion.php");
+error_reporting(E_ERROR | E_PARSE);
+require("./PDF/fpdf186/fpdf.php"); //FPDF
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //Creación del PDF
+    class PDF extends FPDF
+    {
+        function Header() {
+            // Encabezado
+            $this->SetFont('Arial','B',12);
+            $this->Cell(0,10,'Mi PDF',0,1,'C');
+            $this->Ln(10);
+        }
+
+
+        function footer()
+        {
+            $this->SetY(-20);
+            $this->SetFont('times', 'I', '10');
+            $this->Cell(0, 15, 'Pagina '.$this->PageNo().'/{nb}', 0, 0, 'C');
+        }
+    }
+
+    /* $conexion = mysqli_connect("localhost","root","","bdformulario");
+    $consulta = "SELECT * FROM alumno";
+    $resultado = mysqli_query($conexion,$consulta); */
+
     // Verificar si los campos están definidos y no están vacíos
     //Obterer datos de identidad
     $noBoleta = $_POST['NoBoleta'];
@@ -32,7 +57,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $promedio = $_POST['Promedio'];
     $escomOpcion = $_POST['OpRadio'];
 
-
     // Llamar al procedimiento almacenado para obtener el ID del salón
     $sp_salon = 'ObtenerPrimerSalon';
     $stmt = $conexion->prepare("CALL $sp_salon(@salonId)");
@@ -48,38 +72,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $salonRow = $result->fetch_assoc();
     $salonId = $salonRow['salonId'];
 
-/* 
-    // Llamar al procedimiento almacenado
-    $sp_salon = 'ObtenerPrimerSalon';
-    $salon = $conexion->query("CALL $sp_salon(salonId)");
-
-    // Obtener el ID del salón después de llamar al procedimiento
-    $result = $conexion->query("SELECT @salonId AS salonId");
-    $salonRow = $result->fetch_assoc();
-    $salonId = $salonRow['salonId'];
-
-
-    // Verificar si se ejecutó correctamente
-    if ($salon) {
-        echo "Procedimiento almacenado ejecutado con éxito.";
-    } else {
-        echo "Error al ejecutar el procedimiento almacenado: " . $conexion->error;
-    }
-*/
     //insertar en la base de datos
     $sqlAlumno = "INSERT INTO Alumno (NoBoleta,idSalon , Nombre, ApellidoPaterno, ApellidoMaterno, CURP, FechaNacimiento, Genero, Discapacidad, Calle, numeroC, EntidadFederativa, MunicipioAlcaldia, CodigoPostal, Telefono, Correo, EscuelaProcedencia, NombreEscuela, Promedio, ESCOM_Opcion) 
-            VALUES ('$noBoleta', '$salonId' , '$nombre', '$apellidoPaterno', '$apellidoMaterno', '$curp', '$fechaNacimiento', '$genero', '$discapacidad', '$calle', '$numeroC', '$entidadFederativa', '$munAl', '$codigoPostal', '$telefono', '$correo', '$escuelaProcedencia', '$nombreEscuela', '$promedio', '$escomOpcion')";
-
-
+        VALUES ('$noBoleta', '$salonId' , '$nombre', '$apellidoPaterno', '$apellidoMaterno', '$curp', '$fechaNacimiento', '$genero', '$discapacidad', '$calle', '$numeroC', '$entidadFederativa', '$munAl', '$codigoPostal', '$telefono', '$correo', '$escuelaProcedencia', '$nombreEscuela', '$promedio', '$escomOpcion')";
 
     if ($conexion->query($sqlAlumno) !== TRUE) {
         die("Error al insertar datos del alumno: " . $conexion->error);
     }
-    
-    //echo "Datos insertados correctamente en la base de datos.";
-    
 
+    $pdf = new PDF();
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    $pdf->SetFont('arial', 'B', 14);
+    $pdf->Write(50, 'Estimado alumno: '.$nombre);
+    $pdf->Ln(30);
+    $pdf->SetFont('arial', '', 12);
+    $pdf->Cell(0, 10, 'Por medio de este documento');
+    $pdf->Output('I', 'Examen.pdf');
 }
+
 $conexion->close(); // Cerrar la conexión
 
 ?>
+
+
